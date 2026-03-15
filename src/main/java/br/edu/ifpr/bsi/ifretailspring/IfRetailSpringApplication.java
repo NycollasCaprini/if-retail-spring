12b76e46114a5853a2637063e86c4f5b2aedbd49
Luiz Francisco <luizfk.marinho@gmail.com>
2026-03-15 16:51:04 -0300

    configuracao inicial

---
+++ src/main/java/br/edu/ifpr/bsi/ifretailspring/IfRetailSpringApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.bsi.ifretailspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfRetailSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfRetailSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpr/bsi/ifretailspring/IfRetailSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.bsi.ifretailspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfRetailSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
